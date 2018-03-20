5638b91b0e40e47f9a5305eec26bf417da6b35bf
HoustonEB <860852345@qq.com>
2018-03-20 20:35:40 +0800

    u

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@
 $(function () {
 
     $('#fullpage').fullpage({
-        sectionsColor: ['#666', '#fff', '#999','#666'],
+        sectionsColor: ['#333', '#fff', '#fff','#fff'],
         // 导航
         navigation: false,
         scrollBar: true,
+        // 滚动速度
+        scrollingSpeed: 500,
         // 去除箭头
         controlArrows: true
     });
@@ -53,22 +55,22 @@ $(function () {
         }
         console.log(clickTrue);
         setTimeout(function () {
-            $('#aside-nav li').eq(1).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(2).css('transform', 'translateY(0px)');
         }, 100);
         setTimeout(function () {
-            $('#aside-nav li').eq(2).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(3).css('transform', 'translateY(0px)');
         }, 200);
         setTimeout(function () {
-            $('#aside-nav li').eq(3).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(4).css('transform', 'translateY(0px)');
         }, 300);
         setTimeout(function () {
-            $('#aside-nav li').eq(4).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(5).css('transform', 'translateY(0px)');
         }, 400);
         setTimeout(function () {
-            $('#aside-nav li').eq(5).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(6).css('transform', 'translateY(0px)');
         }, 500);
         setTimeout(function () {
-            $('#aside-nav li').eq(6).css('transform', 'translateY(0px)');
+            $('#aside-nav li').eq(7).css('transform', 'translateY(0px)');
             // 此处如果将锁放入外面则先锁后执行定时器-因为定时器没外边的代码执行快要排1
             clickTrue = false;
         }, 600);
@@ -80,26 +82,76 @@ $(function () {
             return;
         }
         setTimeout(function () {
-            $('#aside-nav li').eq(1).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(2).css('transform', 'translateY(-50px)');
         }, 100);
         setTimeout(function () {
-            $('#aside-nav li').eq(2).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(3).css('transform', 'translateY(-50px)');
         }, 200);
         setTimeout(function () {
-            $('#aside-nav li').eq(3).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(4).css('transform', 'translateY(-50px)');
         }, 300);
         setTimeout(function () {
-            $('#aside-nav li').eq(4).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(5).css('transform', 'translateY(-50px)');
         }, 400);
         setTimeout(function () {
-            $('#aside-nav li').eq(5).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(6).css('transform', 'translateY(-50px)');
         }, 500);
         setTimeout(function () {
-            $('#aside-nav li').eq(6).css('transform', 'translateY(-50px)');
+            $('#aside-nav li').eq(7).css('transform', 'translateY(-50px)');
             clickTrue = true;
         }, 600);
     });
 
+
+    // echarts图表
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('pielist'));
+
+    // 指定图表的配置项和数据
+    option = {
+        title: {
+            text: '某站点用户访问来源',
+            subtext: '纯属虚构',
+            x: 'center',
+            y: '100px'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            top: '150px',
+            left: '70px',
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '40%',
+                center: ['50%', '60%'],
+                data: [
+                    { value: 335, name: '直接访问' },
+                    { value: 310, name: '邮件营销' },
+                    { value: 234, name: '联盟广告' },
+                    { value: 135, name: '视频广告' },
+                    { value: 1548, name: '搜索引擎' }
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
 
 
 
